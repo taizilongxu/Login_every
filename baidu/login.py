39cda8654f405f9a6dc392d32816c41cbe5428fc
taizilongxu <468137306@qq.com>
2014-09-20 15:42:12 +0800

    add pan and baidu

---
+++ baidu/login.py
@@ -0,0 +1,118 @@
+#-*- encoding: UTF-8 -*-
+#---------------------------------import------------------------------------
+#---------------------------------------------------------------------------
+#coding:utf-8
+
+#百度登陆更改版
+
+import urllib2,requests,time,re
+
+def login_baidu(name,password):
+    #访问主页，获取cookie
+    s.get('http://www.baidu.com')
+    # s.get('https://passport.baidu.com/v2/api/?login')
+    #获取token 值
+    cook = s.get("https://passport.baidu.com/v2/api/?getapi&class=login&tpl=mn&tangram=true")
+    data = cook.text
+    token = re.findall(r"bdPass.api.params.login_token='(.*?)'",data)[0]
+
+    #构造包的头部
+    headers = {
+    'Host': 'passport.baidu.com',
+    "User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0",
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Referer': 'http://pan.baidu.com/',
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    s.get("https://passport.baidu.com/v2/api/?login",headers=headers)
+
+    #第一次post数据
+    payload={
+            'staticpage':'http://pan.baidu.com/res/static/thirdparty/pass_v3_jump.html',
+            'charset':'utf-8',
+            'token':token,
+            'tpl':'netdisk',
+            'subpro': '',
+            'apiver':'v3',
+            'tt': '1411196003909',
+            'codestring' : '',
+            'safeflg' : '0',
+            'u' :'http://pan.baidu.com/',
+            'isPhone' : 'false',
+            'quick_user'  : '0',
+            'logintype' : 'basicLogin',
+            'logLoginType' : 'pc_loginBasic',
+            'idc' : '',
+            'loginmerge' : 'true',
+            'username': name,
+            'password': password,
+            'verifycode':'',
+            'mem_pass':'on',
+            'ppui_logintime' : '49586',
+            'callback':'parent.bd__pcbs__5s70r4'
+    }
+    #第一次post，获取验证码地址
+    login = s.post("https://passport.baidu.com/v2/api/?login", data=payload,headers=headers,verify=True)
+    get_code = re.findall(r'codeString=(.*?)&userName',login.text)[0]
+    print get_code
+
+    #获取验证码
+    code = s.get("https://passport.baidu.com/cgi-bin/genimage",params=get_code,stream=True)
+    path = "code"  #请自行修改路径
+    if code.status_code == 200:
+        with open(path, 'wb') as f:
+            for chunk in code.iter_content():
+                f.write(chunk)
+
+    #输入验证码
+    verifycode = ''
+    while not verifycode:
+        verifycode = raw_input("请输入验证码:")
+        print verifycode
+
+    #构造post数据
+    payload={
+            'staticpage' : 'http://pan.baidu.com/res/static/thirdparty/pass_v3_jump.html',
+            'charset' : 'utf-8',
+            'token' : token,
+            'tpl':'mn',
+            'apiver':'v3',
+            'tt': '1411196833467',
+            'codestring' : get_code,
+            'safeflg' : '0',
+            'u' :'http://www.baidu.com/',
+            'isPhone' : 'false',
+            'quick_user'  : '0',
+            'logintype' : 'dialogLogin',
+            'logLoginType' : 'pc_loginDialog',
+            'idc' : '',
+            'loginmerge' : 'true',
+            'splogin' : 'rate',
+            'username': name,
+            'password': password,
+            'verifycode':verifycode,
+            'mem_pass':'on',
+            'ppui_logintime' : '49586',
+            'callback':'parent.bd__pcbs__hksq59'
+        }
+
+    login2 = s.post("https://passport.baidu.com/v2/api/?login", data=payload,headers=headers,verify=True)
+
+    #判断是否登录成功,判断cookie中是否含有'BDUSS'
+    if 'BDUSS' in s.cookies:
+        print " 登录成功"
+    else:
+        print "登录失败"
+
+if __name__ == '__main__':
+
+    #构造一个会话，用来跨请求保存cookie
+    s = requests.Session()
+    name = '账号'
+    password = '密码'
+    login_baidu(name,password)
+    print s.get('http://i.baidu.com/').text
+
+############################################################################
---
+++ baidu/wangpan/README.md
@@ -0,0 +1,4 @@
+
+##参考资料
+
+http://www.zhengyanlong.org.cn/programming/python/python-login-baidu-analysis.html
